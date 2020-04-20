6827c12090ffb817820fbb6bb9385dc2c99cd087
himanshumalhotra19 <siddhantbhadsavale@gmail.com>
2020-04-20 11:40:51 +0530

    Clear

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,24 +1,137 @@
-// Credit: This contract is copied and pasted from https://docs.ethers.io/ethers.js/html/api-contract.html. Some necessary modifications for 0.5.11 have been done.
+pragma solidity >=0.4.0 <0.6.0;
 
-// Place your solidity files in this contracts folder and run the compile.js file using node compile.js file in project directory to compile your contracts.
+//Land Details
+contract landRegistration{
+    struct landDetails{
+        string state;
+        string district;
+        string village;
+        uint surveyNumber;
+        address payable CurrentOwner;
+        uint marketValue;
+        bool isAvailable;
+        address requester;
+        reqStatus requestStatus;
 
-pragma solidity ^0.6.3;
+    }
+    
 
-contract SimpleStorage {
-  string value;
+    //request status
+    enum reqStatus {Default,pending,reject,approved}
 
-  event ValueChanged(address indexed author, string oldValue, string newValue);
 
-  constructor(string memory _value) public {
-    setValue(_value);
-  }
 
-  function getValue() view public returns (string memory) {
-    return value;
-  }
+    //profile of a client
+    struct profiles{
+        uint[] assetList;   
+        }
 
-  function setValue(string memory _value) public {
-    emit ValueChanged(msg.sender, value, _value);
-    value = _value;
-  }
+ 
+    mapping(uint => landDetails) land;
+    address owner;
+    mapping(string => address) superAdmin;
+    mapping(address => profiles) profile;
+    
+    //contract owner
+    constructor() public{
+        owner = msg.sender;
+    }
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    //adding village admins
+    function addSuperAdmin(address _superAdmin,string memory _village ) onlyOwner public {
+        superAdmin[_village]=_superAdmin;
+    }
+    //Registration of land details.
+    function Registration(string memory _state,string memory _district,
+        string memory _village,uint _surveyNumber,
+        address payable _OwnerAddress,uint _marketValue,uint id
+        ) public returns(bool) {
+        require(superAdmin[_village] == msg.sender || owner == msg.sender);
+        land[id].state = _state;
+        land[id].district = _district;
+        land[id].village = _village;
+        land[id].surveyNumber = _surveyNumber;
+        land[id].CurrentOwner = _OwnerAddress;
+        land[id].marketValue = _marketValue;
+        profile[_OwnerAddress].assetList.push(id);
+        return true;
+    }
+    //to view details of land for the owner
+    function landInfoOwner(uint id) public view returns(string memory,string memory,string memory,uint256,bool,address,reqStatus){
+        return(land[id].state,land[id].district,land[id].village,land[id].surveyNumber,land[id].isAvailable,land[id].requester,land[id].requestStatus);
+    }
+        //to view details of land for the buyer
+        function landInfoUser(uint id) public view returns(address,uint,bool,address,reqStatus){
+        return(land[id].CurrentOwner,land[id].marketValue,land[id].isAvailable,land[id].requester,land[id].requestStatus);
+    }
+
+    // to compute id for a land.
+    function computeId(string memory _state,string memory _district,string memory _village,uint _surveyNumber) public view returns(uint){
+        return uint(keccak256(abi.encodePacked(_state,_district,_village,_surveyNumber)))%10000000000000;
+    }
+
+    //push a request to the land owner
+    function requstToLandOwner(uint id) public {
+        require(land[id].isAvailable);
+        land[id].requester=msg.sender;
+        land[id].isAvailable=false;
+        land[id].requestStatus = reqStatus.pending; //changes the status to pending.
+    }
+    //will show assets of the function caller 
+    function viewAssets()public view returns(uint[] memory){
+        return (profile[msg.sender].assetList);
+    }
+    //viewing request for the lands
+    function viewRequest(uint property)public view returns(address){
+        return(land[property].requester);
+    }
+    //processing request for the land by accepting or rejecting
+    function processRequest(uint property,reqStatus status)public {
+        require(land[property].CurrentOwner == msg.sender);
+        land[property].requestStatus=status;
+        if(status == reqStatus.reject){
+            land[property].requester = address(0);
+            land[property].requestStatus = reqStatus.Default;
+        }
+    }
+    //availing land for sale.
+    function makeAvailable(uint property)public{
+        require(land[property].CurrentOwner == msg.sender);
+        land[property].isAvailable=true;
+    } 
+    //buying the approved property
+    function buyProperty(uint property)public payable{
+        require(land[property].requestStatus == reqStatus.approved);
+        // require(msg.value >= (land[property].marketValue+((land[property].marketValue)/10)));
+         require(msg.value >= land[property].marketValue);
+
+        land[property].CurrentOwner.transfer(land[property].marketValue);
+        removeOwnership(land[property].CurrentOwner,property);
+        land[property].CurrentOwner=msg.sender;
+        land[property].isAvailable=false;
+        land[property].requester = address(0);
+        land[property].requestStatus = reqStatus.Default;
+        profile[msg.sender].assetList.push(property); //adds the property to the asset list of the new owner.
+        
+    }
+    //removing the ownership of seller for the land. and it is called by the buyProperty function
+    function removeOwnership(address previousOwner,uint id)private{
+        uint index = findId(id,previousOwner);
+        profile[previousOwner].assetList[index]=profile[previousOwner].assetList[profile[previousOwner].assetList.length-1];
+        delete profile[previousOwner].assetList[profile[previousOwner].assetList.length-1];
+        profile[previousOwner].assetList.length--;
+    }
+    //for finding the index of a perticular id
+    function findId(uint id,address user)public view returns(uint){
+        uint i;
+        for(i=0;i<profile[user].assetList.length;i++){
+            if(profile[user].assetList[i] == id)
+                return i;
+        }
+        return i;
+    }
 }
--- test/SimpleStorage.test.js
+++ test/SimpleStorage.test.js
@@ -1,6 +1,5 @@
 /*
   Author: Soham Zemse (https://github.com/zemse)
-
   In this file you should write tests for your smart contract as you progress in developing your smart contract. For reference of Mocha testing framework, you can check out https://devdocs.io/mocha/.
 */
 
@@ -14,19 +13,19 @@ const { parseTx } = require('../helpers');
 const DEBUG_MODE = false;
 
 /// @dev initialising development blockchain
-const provider = new ethers.providers.Web3Provider(ganache.provider({ gasLimit: 8000000 }));
+const provider = new ethers.providers.Web3Provider(ganache.provider({ gasLimit: 80000000 }));
 
 /// @dev importing build file
-const simpleStorageJSON = require('../build/SimpleStorage_SimpleStorage.json');
+const simpleStorageJSON = require('../build/SimpleStorage_landRegistration.json');
 
 /// @dev initialize global variables
 let accounts, simpleStorageInstance;
-
+let mValue;
 /// @dev this is a test case collection
 describe('Ganache Setup', async() => {
 
   /// @dev this is a test case. You first fetch the present state, and compare it with an expectation. If it satisfies the expectation, then test case passes else an error is thrown.
-  it('initiates ganache and generates a bunch of demo accounts', async() => {
+  it('initiates ganache and generates a bunch of demo accounts..Like a local blockchain', async() => {
 
     /// @dev for example in this test case we are fetching accounts array.
     accounts = await provider.listAccounts();
@@ -37,13 +36,13 @@ describe('Ganache Setup', async() => {
 });
 
 /// @dev this is another test case collection
-describe('Simple Storage Contract', () => {
+describe('Land Registration Contract', () => {
 
   /// @dev describe under another describe is a sub test case collection
-  describe('Simple Storage Setup', async() => {
+  describe('Land Registration Setup', async() => {
 
     /// @dev this is first test case of this collection
-    it('deploys Simple Storage contract from first account with initial storage: Hello World', async() => {
+    it('deploys Simple Storage contract from first account with initial storage: landRegistration', async() => {
 
       /// @dev you create a contract factory for deploying contract. Refer to ethers.js documentation at https://docs.ethers.io/ethers.js/html/
       const SimpleStorageContractFactory = new ethers.ContractFactory(
@@ -51,45 +50,131 @@ describe('Simple Storage Contract', () => {
         simpleStorageJSON.evm.bytecode.object,
         provider.getSigner(accounts[0])
       );
-      simpleStorageInstance =  await SimpleStorageContractFactory.deploy('hello world');
+      simpleStorageInstance =  await SimpleStorageContractFactory.deploy();
 
       assert.ok(simpleStorageInstance.address, 'conract address should be present');
     });
 
     /// @dev this is second test case of this collection
-    it('value should be set properly while deploying', async() => {
+    // it('value should be set properly while deploying', async() => {
 
-      /// @dev you access the value at storage with ethers.js library of our custom contract method called getValue defined in contracts/SimpleStorage.sol
-      const currentValue = await simpleStorageInstance.functions.getValue();
+    //   /// @dev you access the value at storage with ethers.js library of our custom contract method called getValue defined in contracts/SimpleStorage.sol
+    //   const currentValue = await simpleStorageInstance.functions.getValue();
 
-      /// @dev then you compare it with your expectation value
-      assert.equal(
-        currentValue,
-        'hello world',
-        'value set while deploying must be visible when get'
-      );
-    });
+    //   /// @dev then you compare it with your expectation value
+    //   assert.equal(
+    //     currentValue,
+    //     'hello world',
+    //     'value set while deploying must be visible when get'
+    //   );
+    // });
   });
 
-  describe('Simple Storage Functionality', async() => {
+  describe('Land Registration Functionality', async  => {
 
     /// @dev this is first test case of this collection
-    it('should change storage value to a new value', async() => {
-
-      /// @dev you sign and submit a transaction to local blockchain (ganache) initialized on line 10.
-      ///   you can use the parseTx wrapper to parse tx and output gas consumption and logs.
-      ///   use parseTx with non constant methods
-      const receipt = await parseTx(simpleStorageInstance.functions.setValue('hi'), DEBUG_MODE);
+    it('should compute the Id', async() => {
 
       /// @dev now get the value at storage
-      const currentValue = await simpleStorageInstance.functions.getValue();
+      const currentValue = await simpleStorageInstance.functions.computeId('','','',1);
 
       /// @dev then comparing with expectation value
       assert.equal(
         currentValue,
-        'hi',
-        'value set must be able to get'
+        9459944778998
       );
     });
+    it("Registering the land by accounts[0]", async() => {
+      let id = await simpleStorageInstance.functions.computeId('maharashtra','thane','kalyan',21);
+      let register = await parseTx(simpleStorageInstance.functions.Registration('maharashtra','thane','kalyan',21,accounts[1],20,id), DEBUG_MODE)
+      assert(register);
+
+        //     const receipt = await parseTx(instance.functions.setValue('hi'), DEBUG_MODE);
+
+    });
+
   });
+  describe('Checking successful LandTransaction',async()=>{
+    it("checking successful registration",async()=>{
+      let value = 20;
+
+      let marketValue = ethers.utils.parseEther(value.toString());
+      mValue = marketValue;
+      let id = await simpleStorageInstance.functions.computeId('maharashtra','thane','kalyan',21);
+       await parseTx(simpleStorageInstance.functions.Registration('maharashtra','thane','kalyan',21,accounts[1],marketValue,id), DEBUG_MODE)
+      let landInfo = await simpleStorageInstance.functions.landInfoUser(id);
+      assert.equal(landInfo[0],accounts[1]);
+    });
+    it("checking the availability before making available, by default it is unavailable",async()=>{
+      let id = await simpleStorageInstance.functions.computeId('maharashtra','thane','kalyan',21);
+      let landInfo = await simpleStorageInstance.landInfoUser(id);
+      assert.equal(landInfo[2],false);
+
+    })
+    it("checking that the buyer can only make request if the property is available for sale", async() => {
+      let id = await simpleStorageInstance.functions.computeId('maharashtra','thane','kalyan',21);
+      let state;
+      try {
+        let instance1 = simpleStorageInstance.connect(provider.getSigner(accounts[1]))
+
+        await instance1.functions.requstToLandOwner(id);
+        state =false;
+
+      } catch (error) {
+        state = true;
+
+      }      
+      assert(state);
+    })
+    it("checking the availability for buying a land after make it avialable", async() => {
+      let id = await simpleStorageInstance.functions.computeId('maharashtra','thane','kalyan',21);
+     let instance1 = simpleStorageInstance.connect(provider.getSigner(accounts[1]))
+     let instance2 = simpleStorageInstance.connect(provider.getSigner(accounts[2]))
+     await instance1.functions.makeAvailable(id);
+      let landInfo = await instance2.functions.landInfoUser(id);
+      assert.equal(landInfo[2],true);
+    })
+
+    it("checking that the request for land works!!", async() => {
+      let id = await simpleStorageInstance.functions.computeId('maharashtra','thane','kalyan',21);
+      let instance2 = simpleStorageInstance.connect(provider.getSigner(accounts[2]))
+      await instance2.functions.requstToLandOwner(id);
+      let landInfo = await instance2.functions.landInfoUser(id);
+      assert.equal(landInfo[3],accounts[2])
+    });
+    it("checking that the buyer can only buy, if the request is approved by the owner", async() => {
+      let id = await simpleStorageInstance.functions.computeId('maharashtra','thane','kalyan',21);
+      let state;
+      try {
+        let instance2 = simpleStorageInstance.connect(provider.getSigner(accounts[2]))
+        await instance2.functions.buyProperty(id,{value : mValue });
+        state = false;
+      } catch (error) {
+        state = true;
+
+      }
+      assert(state);
+    });
+    it("checking that the request approval works!!", async() => {
+      let id = await simpleStorageInstance.functions.computeId('maharashtra','thane','kalyan',21);
+
+      let instance1 = simpleStorageInstance.connect(provider.getSigner(accounts[1]))
+      let instance2 = simpleStorageInstance.connect(provider.getSigner(accounts[2]))
+      await instance1.functions.processRequest(id,3);
+      let landInfo = await instance2.landInfoUser(id);
+      assert.equal(landInfo[4],3)
+
+    })
+    it("checking that the buyer can buy the property and the ownership changes", async() => {
+      let id = await simpleStorageInstance.functions.computeId('maharashtra','thane','kalyan',21);
+      let instance2 = simpleStorageInstance.connect(provider.getSigner(accounts[2]))
+      await instance2.functions.buyProperty(id,{value : mValue});
+      let landInfo = await instance2.landInfoUser(id);
+      assert.equal(landInfo[0],accounts[2]);
+
+    })
+
+  });
+  
 });
+
